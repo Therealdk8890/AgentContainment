e3588708cbe724330fea208adbc94c03199f9e4a
Danny <therealdk8890@gmail.com>
2026-09-23 07:28:37 -0500

    Expose controller service API

--- src/agent_containment/__init__.py
+++ src/agent_containment/__init__.py
@@ -1,3 +1,4 @@
+from .control import ContainmentService, ManagedAgent
 from .egress import EgressController, EgressLease, hard_close_socket
 from .gateway import ActionGateway
 from .models import Action, Decision, DecisionType
@@ -5,5 +6,6 @@ from .policy import PolicyEngine
 
 __all__ = [
     "Action", "ActionGateway", "Decision", "DecisionType",
+    "ContainmentService", "ManagedAgent",
     "EgressController", "EgressLease", "PolicyEngine", "hard_close_socket",
 ]
