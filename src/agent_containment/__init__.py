cc43048b25a15e33c1b42a68f3042c0e656a8778
Danny <therealdk8890@gmail.com>
2026-09-23 12:37:17 -0500

    Export Linux cgroup supervisor

--- src/agent_containment/__init__.py
+++ src/agent_containment/__init__.py
@@ -1,6 +1,7 @@
 from .control import ContainmentService, ManagedAgent
 from .egress import EgressController, EgressLease, hard_close_socket
 from .gateway import ActionGateway
+from .linux_supervisor import LinuxCgroupSupervisor
 from .models import Action, Decision, DecisionType
 from .policy import PolicyEngine
 
@@ -8,4 +9,5 @@ __all__ = [
     "Action", "ActionGateway", "Decision", "DecisionType",
     "ContainmentService", "ManagedAgent",
     "EgressController", "EgressLease", "PolicyEngine", "hard_close_socket",
+    "LinuxCgroupSupervisor",
 ]
