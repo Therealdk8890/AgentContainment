e1591fef579ea5c961bbb02bb9aae38109724052
Danny <therealdk8890@gmail.com>
2026-09-23 20:33:46 -0500

    Export recovery authorization API

--- src/agent_containment/__init__.py
+++ src/agent_containment/__init__.py
@@ -1,4 +1,4 @@
-from .control import ContainmentService, ManagedAgent
+from .control import ContainmentService, ManagedAgent, RecoveryAuthorization
 from .egress import EgressController, EgressLease, hard_close_socket
 from .gateway import ActionGateway
 from .incident_state import IncidentRecord, IncidentRegistry, IncidentState
@@ -8,7 +8,7 @@ from .policy import PolicyEngine
 
 __all__ = [
     "Action", "ActionGateway", "Decision", "DecisionType",
-    "ContainmentService", "ManagedAgent",
+    "ContainmentService", "ManagedAgent", "RecoveryAuthorization",
     "IncidentRecord", "IncidentRegistry", "IncidentState",
     "EgressController", "EgressLease", "PolicyEngine", "hard_close_socket",
     "LinuxCgroupSupervisor",
