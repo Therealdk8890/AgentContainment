a52893857c66653b311bcd641e31d5892f540951
Danny <therealdk8890@gmail.com>
2026-09-24 12:43:56 -0500

    Export enforcement boundary API

--- src/agent_containment/__init__.py
+++ src/agent_containment/__init__.py
@@ -1,5 +1,6 @@
 from .control import ContainmentService, ManagedAgent, RecoveryAuthorization
 from .egress import EgressController, EgressLease, hard_close_socket
+from .enforcer import Enforcer, EnforcementResult, EnforcementStatus, NoopEnforcer
 from .gateway import ActionGateway
 from .incident_state import IncidentRecord, IncidentRegistry, IncidentState
 from .linux_supervisor import LinuxCgroupSupervisor
@@ -13,4 +14,5 @@ __all__ = [
     "IncidentRecord", "IncidentRegistry", "IncidentState",
     "EgressController", "EgressLease", "PolicyEngine", "hard_close_socket",
     "LinuxCgroupSupervisor", "FenceRecord", "RuntimeFenceRegistry",
+    "Enforcer", "EnforcementResult", "EnforcementStatus", "NoopEnforcer",
 ]
