800aa345acc09ac788eb6ca3476a5dc26e4a5116
Danny <therealdk8890@gmail.com>
2026-09-24 23:24:46 -0500

    Export Warden observation contract

--- src/agent_containment/__init__.py
+++ src/agent_containment/__init__.py
@@ -6,11 +6,11 @@ from .governance_event import GovernanceEvent
 from .enforcer import Enforcer, EnforcementResult, EnforcementStatus, NoopEnforcer
 from .gateway import ActionGateway
 from .incident_state import IncidentRecord, IncidentRegistry, IncidentState
-from .governance_event import GovernanceEvent
 from .linux_supervisor import LinuxCgroupSupervisor
 from .models import Action, Decision, DecisionType
 from .policy import PolicyEngine
 from .runtime_fence import FenceRecord, RuntimeFenceRegistry
+from .warden_observation import WardenObservation
 
 __all__ = [
     "Action", "ActionGateway", "Decision", "DecisionType",
@@ -19,5 +19,5 @@ __all__ = [
     "EgressController", "EgressLease", "PolicyEngine", "hard_close_socket",
     "LinuxCgroupSupervisor", "FenceRecord", "RuntimeFenceRegistry",
     "Enforcer", "EnforcementResult", "EnforcementStatus", "NoopEnforcer",
-    "CgroupV2Enforcer", "CiliumNetworkPolicyEnforcer", "GovernanceEvent",
+    "CgroupV2Enforcer", "CiliumNetworkPolicyEnforcer", "WardenObservation",
 ]
