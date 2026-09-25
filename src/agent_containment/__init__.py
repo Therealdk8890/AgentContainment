7729fa06cf305110595d0b16bf81d23bf397c7c5
Danny <therealdk8890@gmail.com>
2026-09-24 19:55:45 -0500

    Export governance event API

--- src/agent_containment/__init__.py
+++ src/agent_containment/__init__.py
@@ -2,6 +2,7 @@ from .control import ContainmentService, ManagedAgent, RecoveryAuthorization
 from .cgroup_enforcer import CgroupV2Enforcer
 from .cilium_enforcer import CiliumNetworkPolicyEnforcer
 from .egress import EgressController, EgressLease, hard_close_socket
+from .governance_event import GovernanceEvent
 from .enforcer import Enforcer, EnforcementResult, EnforcementStatus, NoopEnforcer
 from .gateway import ActionGateway
 from .incident_state import IncidentRecord, IncidentRegistry, IncidentState
@@ -17,5 +18,5 @@ __all__ = [
     "EgressController", "EgressLease", "PolicyEngine", "hard_close_socket",
     "LinuxCgroupSupervisor", "FenceRecord", "RuntimeFenceRegistry",
     "Enforcer", "EnforcementResult", "EnforcementStatus", "NoopEnforcer",
-    "CgroupV2Enforcer", "CiliumNetworkPolicyEnforcer",
+    "CgroupV2Enforcer", "CiliumNetworkPolicyEnforcer", "GovernanceEvent",
 ]
